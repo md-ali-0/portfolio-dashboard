b6d7e1e322f8802707ef2b35f96d5878147b0c9c
md-ali-0 <mohammad.98482@gmail.com>
2026-03-30 12:19:37 +0600

    update: latest changes

--- src/components/ui/rich-text-editor.tsx
+++ src/components/ui/rich-text-editor.tsx
@@ -59,7 +59,7 @@ export default function RichTextEditor({
                 images_upload_url: "/api/upload",
                 branding: false,
                 promotion: false,
-                skin_url: "/tinymce/skins/ui/oxide-dark",
+                skin_url: "/tinymce/skins/ui/tinymce-5-dark",
                 content_css: "/tinymce/skins/content/dark/content.min.css",
                 content_style:
                     "body { font-family:Inter,Helvetica,Arial,sans-serif; font-size:14px; background-color:#18181b; color:#f4f4f5; }",
